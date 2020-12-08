a0d230e3f9e0e1dbe943b85847f40b3d3bbc54da
Sandro Peirano <sandro.peirano@gdpteam.com>
2020-12-08 12:53:55 -0500

    Succesfully configured tailwind and angular

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+// Boilerplate & environment
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 
-// firebase related modules
+// Firebase related modules
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
@@ -13,8 +15,12 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirebaseService } from './core/firebase/firebase.service';
 import { RootStoreModule } from './core/store';
 
-// debug
+// Sharedb modules
+import { MaterialModule } from 'src/shared/material/material.module';
+
+// Debug
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,7 +32,9 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     RootStoreModule,
     StoreDevtoolsModule.instrument({
       maxAge: 25,
-    })
+    }),
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [FirebaseService],
   bootstrap: [AppComponent],
